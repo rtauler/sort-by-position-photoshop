6b93876c235e1b7ad504d3bebee4dcc1ccdf3474
rtauler <rtauler@gmail.com>
2017-06-07 12:27:56 +0200

    working version, without external library only top layers

--- [RTCC] Sort by position.jsx
+++ [RTCC] Sort by position.jsx
@@ -1,31 +1,14 @@
-//@include "/Applications/Adobe Photoshop CC 2017/Presets/Scripts/SelectedLayers.jsx"
+// sort the layers by name
+for (var x = 0; x < app.activeDocument.layers.length; x++) {
+    for (var y = 0; y < app.activeDocument.layers.length - 1 - x; y++) { // Compare in a non-case sensitive way
+    	// check wich top px position has the layer
+        var doc1 = app.activeDocument.layers[y].bounds[1];
+        var doc2 = app.activeDocument.layers[y + 1].bounds[1];
+        // test if the previous and next layer are > or <
+        if (parseInt(doc1) < parseInt(doc2)) { app.activeDocument.layers[y].move(app.activeDocument.layers[y+1],
+            ElementPlacement.PLACEAFTER)
+    } 
 
-
-var layers = getSelectedLayers(app.activeDocument);
-var originalNames = [];
-var layersArray = [];
-
-app.activeDocument.suspendHistory("Sort Layers by Position", "sortLayers();" );
-
-function sortLayers(){
-	for (i = 0; i < layers.length; i++){
-		originalNames.push(layers[i].name);
-		layers[i].name = layers[i].bounds[1];
-		layersArray.push(layers[i]);    
-	}
-
-	alert(layersArray);
-
-	layersArray.sort(function(a,b) {
-		return parseInt(a.name,10)-parseInt(b.name,10);
-	});
-
-	alert(layersArray);
-
-	for (i = 0; i < layers.length; i++){
-		layersArray[i].move(layers[i], ElementPlacement.PLACEBEFORE);
-	layers[i].name = originalNames[i];
-
-	}
+}
 }
 
